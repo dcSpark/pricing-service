8be156861d26aa4086e63c5cf452b1c2b9060d71
Jakub Smahovsky <jakubsmahovsky@gmail.com>
2023-01-30 19:57:25 +0100

    increase refresh intervals
    also load config form env if present

--- config/default.ts
+++ config/default.ts
@@ -1,11 +1,11 @@
-export default ({ 
+export default {
   priceAPI: {
-    url: process.env.CRYPTOCOMPARE_URL || "https://min-api.cryptocompare.com",
-    key: process.env.CRYPTOCOMPARE_KEY || ""
+    url: process.env.CRYPTOCOMPARE_URL ?? "https://min-api.cryptocompare.com",
+    key: process.env.CRYPTOCOMPARE_KEY ?? "",
   },
   APIGenerated: {
-    refreshBackoffCap: 60000*60*3,
-    refreshInterval: 60000, // refreshes once per min
-    port: 8090 
-  }
-} as ConfigType);
+    refreshBackoffCap: process.env.REFRESH_BACKOFF_CAP ?? 60000 * 60 * 3,
+    refreshInterval: process.env.REFRESH_INTERVAL ?? 60000 * 5, // refreshes every 5 mins
+    port: process.env.PORT ?? 8090,
+  },
+} as ConfigType;
